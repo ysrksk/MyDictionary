fab8baf0b8e76a2aaf882ce9ccade1163b167c8f
Yaşar Küskü <yasarkusku@hotmail.com>
2022-07-10 11:48:40 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Ankara");
+            myDictionary.Add("İstanbul");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
